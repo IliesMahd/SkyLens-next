1db9b1c481d406fd90f4ad3211bcb3ae834fc7f5
Ilies Mahoudeau <mahoudea.i@sio.local>
2023-12-14 09:15:29 +0100

    création projet next + premiers composants

---
+++ skylens/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Footer from "./components/Layout/Footer";
+import { Header } from "./components/Layout/Header";
+import Routes from "./components/utils/Routes";
+
+export function Home() {
+  return (
+    <>
+      <Header/>
+      <Routes/>
+      <Footer/>
+    </>
+  )
+}
